8bd9eea9681ea4ff9d49bba284eef4fa206382e5
Evan Baisden <ehbaisden@gmail.com>
2024-10-21 16:19:58 -0400

    new files

--- src/data/data_collection.py
+++ src/data/data_collection.py
@@ -1,12 +1,11 @@
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
-import config
+import os
+from dotenv import load_dotenv
 
-def fetch_stock_data(symbol):
-    ts = TimeSeries(key=config.ALPHA_VANTAGE_API_KEY, output_format='pandas')
-    data, meta_data = ts.get_daily_adjusted(symbol=symbol, outputsize='full')
-    return data
+load_dotenv()  # Make sure this line is present
 
-if __name__ == "__main__":
-    nvda_data = fetch_stock_data('NVDA')
-    nvda_data.to_csv('../../data/raw/NVDA_stock_data.csv')
+# For debugging purposes
+print("FRED_API_KEY:", os.getenv('FRED_API_KEY'))
+print("NEWS_API_KEY:", os.getenv('NEWS_API_KEY'))
+print("MARKETSTACK_API_KEY:", os.getenv('MARKETSTACK_API_KEY'))
+print("ALPHA_VANTAGE_API_KEY:", os.getenv('ALPHA_VANTAGE_API_KEY'))
+print("TIINGO_API_KEY:", os.getenv('TIINGO_API_KEY'))
